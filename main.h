f3a87964a4a36cc15ac7680b734dae12b3ddb46c
Ezio-33 <8691@holbertonstudents.com>
2024-04-19 11:35:58 +0200

    C - Simple Shell : main.h creation

--- main.h
+++ main.h
@@ -1,13 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Variable globale */
+
+#define SÉPARATEURS_ARG " \t\r\n\a\""
+#define ENV_SÉPARATEUR "="
+#define PATH_SÉPARATEUR ":"
+
 /* Librairies */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /*Prototypes */
+char *lire_ligne(void);
+char **diviser_ligne(char *ligne, char *séparateurs);
 
 #endif
